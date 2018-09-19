e8bd394bba64a8dba3524c73c26018bea640f047
MissCA <31779111+MissCA@users.noreply.github.com>
2018-09-19 13:40:14 -0700

    Create Gradebook.java

---
+++ Gradebook.java
@@ -0,0 +1,35 @@
+/*
+Kaitlin Tan
+Chloe Arranza
+9/19/2018
+Gradebook
+*/
+
+public class Gradebook {
+
+  public static void main(String[] args) {
+    System.out.println("Welcome to the Student Gradebook, what would you like to see?");
+    System.out.println("1. Mean grade \n 2. Median Grade \n 3. Top Grade \n 4. Lowest Grade \n 5. Class Subject and Teacher \n 6. Student Roster");
+
+    String userIn = newIn.nextLine();
+    if(userIn.equals("1")){
+      System.out.println("The mean grade is " + );
+    }
+    if(userIn.equals("2")){
+      System.out.println("The median grade is " + );
+    }
+    if(userIn.equals("3")){
+      System.out.println("The top grade is " + );
+    }
+    if(userIn.equals("4")){
+      System.out.println("The lowest grade is " + );
+    }
+    if(userIn.equals("5")){
+      System.out.println("The class subject and Teacher is " + );
+    }
+    if(userIn.equals("6")){
+      System.out.println("The student roster consists of " + );
+    }
+
+  }
+}
