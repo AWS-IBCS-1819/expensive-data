f80a72a680476a086dc76f4ffbbbce64b5b2e570
MissCA <31779111+MissCA@users.noreply.github.com>
2018-09-21 13:01:26 -0700

    Update Gradebook.java

--- Gradebook.java
+++ Gradebook.java
@@ -1,22 +1,18 @@
-/*
-Kaitlin Tan
-Chloe Arranza
-9/19/2018
-Gradebook
-*/
+import java.util.*;
+import java.io.*;
 
 public class Gradebook {
 
   public static void main(String[] args) {
     System.out.println("Welcome to the Student Gradebook, what would you like to see?");
-    System.out.println("1. Mean grade \n 2. Median Grade \n 3. Top Grade \n 4. Lowest Grade \n 5. Class Subject and Teacher \n 6. Student Roster");
+    System.out.println("1. Mean grade \n 2. Grade Spread \n 3. Top Grade \n 4. Lowest Grade \n 5. Class Subject and Teacher \n 6. Student Roster");
 
     String userIn = newIn.nextLine();
     if(userIn.equals("1")){
       System.out.println("The mean grade is " + );
     }
     if(userIn.equals("2")){
-      System.out.println("The median grade is " + );
+      System.out.println("The grade spread is " + );
     }
     if(userIn.equals("3")){
       System.out.println("The top grade is " + );
