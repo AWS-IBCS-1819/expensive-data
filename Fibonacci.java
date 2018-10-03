3a462433b1f5f58dee0af2bfe278af4cf891b825
MissCA <31779111+MissCA@users.noreply.github.com>
2018-10-03 13:43:04 -0700

    Create Fibonacci.java

---
+++ Fibonacci.java
@@ -0,0 +1,29 @@
+/*
+Chloe Arranza
+Fibonacci (n)
+
+You must return the nth Fibonacci number
+
+Fibonacci Sequence
+
+1,1,2,3,5,8,13...
+
+*/
+
+public class Fibonacci{
+
+  static int fib(int n)
+   {
+   if (n <= 1)
+
+      return n;
+
+   return fib(n-1) + fib(n-2);
+   }
+
+   public static void main (String args[]){
+   int n = 2;
+   System.out.println(fib(n));
+   }
+
+}
